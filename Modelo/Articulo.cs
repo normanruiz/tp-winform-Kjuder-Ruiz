7f41b718ea2a9ba69781e659c68666f954397b09
Norman Ruiz <norman.ruiz@outlook.com.ar>
2022-09-06 09:20:36 -0300

    Agrego al modelo la clase Articulo.

---
+++ Modelo/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public string ImagenUrl { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}

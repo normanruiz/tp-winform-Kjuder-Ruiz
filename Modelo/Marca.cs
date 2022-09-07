c6392deb61a6694bfb6f2fde260d654911601719
Juan Pablo Kjuder <Juampablokjuder@outlook.com.ar>
2022-09-07 20:37:49 -0300

    Agrego la clase Marca

---
+++ Modelo/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+     class Marca
+    {
+        public int Id { get; set; }
+        public int Descripcion { get; set; }
+
+    }
+}
